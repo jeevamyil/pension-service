95098a1aece01e5e84eeb471b79a84036b548651
jeevamyil <jeevastrongest@gmail.com>
2022-05-31 18:00:19 +0530

    pom changes and add eureka client

--- src/main/java/com/pensionDetail/PensionDetailServiceApplication.java
+++ src/main/java/com/pensionDetail/PensionDetailServiceApplication.java
@@ -2,9 +2,11 @@
 package com.pensionDetail;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PensionDetailServiceApplication {
 
 	public static void main(String[] args) {
